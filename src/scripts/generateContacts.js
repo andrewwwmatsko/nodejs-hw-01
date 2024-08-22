b26fd288657e06de3fed30b868d140c273394cd4
andrewwwmatsko <matskoandrew@gmail.com>
2024-08-22 16:11:52 +0300

    PATH to DB, get all, generate methods

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,18 @@
+import fs from 'node:fs/promises';
+
 import { PATH_DB } from '../constants/contacts.js';
 
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+import { getAllContacts } from './getAllContacts.js';
+
+const generateContacts = async (number) => {
+  const contacts = await getAllContacts();
+  const generatedContacts = Array(number).fill(0).map(createFakeContact);
+
+  const newContactList = [...contacts, ...generatedContacts];
+
+  await fs.writeFile(PATH_DB, JSON.stringify(newContactList, null, 2), 'utf-8');
+};
 
 generateContacts(5);
